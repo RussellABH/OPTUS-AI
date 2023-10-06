afaf8ed03417100d8984adf01c886ede1ff47ac6
taylon4 <nstaylor03@gmail.com>
2023-10-06 16:59:46 -0400

    start of parsing comments

--- analysis/data-collection/scrape_drugs_forums.py
+++ analysis/data-collection/scrape_drugs_forums.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 base_link = r'https://drugs-forum.com/forums/opiates-opioids.33/'
 
 headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0.'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0.'
 }
 
-content = requests.get(base_link,headers=headers).content
+content = requests.get(base_link, headers=headers).content
 
 soup = BeautifulSoup(content, 'html.parser')
 # get table containing different drug sections
 table = soup.select_one('ol.nodeList.sectionMain')
-forum_links = [] # store links to start of discussion forums
+forum_links = []  # store links to start of discussion forums
 for section in table.find_all('li'):
     forum_links.append(r'https://drugs-forum.com/' + section.find('a')['href'].strip())
 
@@ -21,15 +21,17 @@ print("Looking at secion: " + url)
 page_content = requests.get(url, headers=headers).content
 page_soup = BeautifulSoup(page_content, 'html.parser')
 
+
 # return max amount of pages from Beautiful Soup
 def getMaxPages(soup: BeautifulSoup) -> int:
     elements = soup.find_all("span", {"class": 'pageNavHeader'})
     # 1 page if there is no page nav bar
     if len(elements) == 0:
         return 1
-    text = elements[1].text # "Page 1 of X"
+    text = elements[1].text  # "Page 1 of X"
     max_pages = int(text.split(" ")[-1])
     return max_pages
+
 
 # parse a section, gathering data from every post once we know page count
 # url is the base url for the site, e.g.
@@ -38,7 +40,8 @@ def parseSection(max_pages: int, url: str):
     data = []
     for i in range(1, 1):
         page_url = url + "page-" + i
-        parsePage(page_url, data) # parse each page
+        parsePage(page_url, data)  # parse each page
+
 
 # parse through entire page of posts in a section
 # example page - https://drugs-forum.com/forums/buprenorphine.406/page-2
@@ -47,11 +50,12 @@ def parsePage(url: str, data: list):
     soup = BeautifulSoup(content, 'html.parser')
 
     # last table are the rows containing the right posts
-    table = soup.find_all('ol', {'class':'discussionListItems'})[-1]
+    table = soup.find_all('ol', {'class': 'discussionListItems'})[-1]
     for row in table.find_all('li'):
         a_tags = row.find_all('h3')[0].find_all('a')
         # last 'a' tag contains href
         post_url = 'https://drugs-forum.com/' + a_tags[-1]['href']
+
 
 # extract all comments from the post in all of its pages
 # example page - https://drugs-forum.com/threads/how-to-start-a-liquid-taper-for-buprenorphine.355158/
@@ -59,7 +63,16 @@ def parsePost(url: str):
     content = requests.get(url, headers=headers).content
     soup = BeautifulSoup(content, 'html.parser')
     num_pages = getMaxPages(soup)
-    for i in range(1, num_pages+1):
-        page_content = requests.get(url + 'page-1').content
+    for i in range(1, num_pages + 1):
+        page_content = requests.get(url + f'page-{i}').content
         soup = BeautifulSoup(page_content, 'html.parser')
-        # TODO - Extract features
+        comments = soup.find('ol').find_all('li')
+        for comment in comments:
+            data = parseComments()
+
+# parse a singular comment in a forum post
+def parseComments(Beautiful: soup) -> list:
+
+    return []
+
+parsePost('https://drugs-forum.com/threads/another-forum-that-can-be-useful.357916/')
