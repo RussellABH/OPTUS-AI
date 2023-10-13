bc5ca309a18043c18468817753332e2e69749a7e
macudude <105890189+macudude@users.noreply.github.com>
2023-10-13 17:39:38 -0400

    Installed Material UI

--- optus-ai/src/Rehab.js
+++ optus-ai/src/Rehab.js
@@ -1,11 +1,12 @@
 import React  from 'react';
 import './Rehab.css';
-
+import SearchIcon from '@mui/icons-material/Search';
 const Rehab = () => {
   return (
       <header className="title">
         <p>Rehab Centers</p>
         <p>I am seeking help for Heroin Addiction in New York.</p>
+        <SearchIcon />
       </header>
   )
 }
