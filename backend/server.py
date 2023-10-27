a806f6f4519311592309a075bfe5bea79d9e098d
taylon4 <nstaylor03@gmail.com>
2023-10-27 17:07:45 -0400

    create new database for rehab centers

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from fastapi.responses import PlainTextResponse
 import os
 
 load_dotenv() # take environment variables from .env.
@@ -9,21 +10,24 @@ app = FastAPI() # initialize fast api server
 
 # pulls from .env file (needs to be in directory you run uvicorn from)
 CONNECTION_STRING = os.getenv("CONNECTION_STRING")
-# Find necessary database collection from airbnb
+# Find necessary database collection
 client = MongoClient(CONNECTION_STRING)
-dbname = client['sample_airbnb']
-collection = dbname['listingsAndReviews']
+dbname = client['optus_ai']
+collection = dbname['rehab_centers']
 
 @app.get("/") # default root
 async def root():
     return {"message": "Hello World"}
 
-# test the mongodb server, extract airbnb data and display it as text
-@app.get("/testdb")
+# test the mongodb server, extract rehab data and display it as text
+@app.get("/rehab", response_class=PlainTextResponse)
 async def db():
     items = collection.find() # list of all entries in collection
     item = items[0] # one entry, dict format
     string = ''
+    string += item['name'] + '\n'
+    string += item['formatted_address'] + '\n'
+    string += item['rating'] + ' stars \n'
     for key, value in item.items():
-        string += "" + str(key) + "" + str(value) + "\n"
+        string += "" + str(key) + " " + str(value) + "\n"
     return string
