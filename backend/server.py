289fdbda8e18952bb9e93a770e5913665b4569c7
taylon4 <nstaylor03@gmail.com>
2023-10-27 17:20:31 -0400

    return sample output for rehab

--- backend/server.py
+++ backend/server.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 from fastapi.responses import PlainTextResponse
 import os
 
-load_dotenv() # take environment variables from .env.
+load_dotenv()  # take environment variables from .env.
 
-app = FastAPI() # initialize fast api server
+app = FastAPI()  # initialize fast api server
 
 # pulls from .env file (needs to be in directory you run uvicorn from)
 CONNECTION_STRING = os.getenv("CONNECTION_STRING")
@@ -15,19 +15,19 @@ client = MongoClient(CONNECTION_STRING)
 dbname = client['optus_ai']
 collection = dbname['rehab_centers']
 
-@app.get("/") # default root
+
+@app.get("/")  # default root
 async def root():
     return {"message": "Hello World"}
+
 
 # test the mongodb server, extract rehab data and display it as text
 @app.get("/rehab", response_class=PlainTextResponse)
 async def db():
-    items = collection.find() # list of all entries in collection
-    item = items[0] # one entry, dict format
+    items = collection.find()  # list of all entries in collection
+    item = items[0]  # one entry, dict format
     string = ''
     string += item['name'] + '\n'
     string += item['formatted_address'] + '\n'
-    string += item['rating'] + ' stars \n'
-    for key, value in item.items():
-        string += "" + str(key) + " " + str(value) + "\n"
+    string += str(item['rating']) + ' stars \n'
     return string
