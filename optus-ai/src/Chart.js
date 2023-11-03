b324a0a25182c574cd2551303eabf02f0ed2f168
macudude <105890189+macudude@users.noreply.github.com>
2023-11-03 17:26:52 -0400

    Created Visualization 1
    -created first visualization: bar chart of word frequencies for January 2011

--- optus-ai/src/Chart.js
+++ optus-ai/src/Chart.js
@@ -1,9 +1,56 @@
-import React, {Component} from 'react';
-import * as d3 from "d3";
 
-class BarChart extends Component 
-{
+//Bar Chart of the frequency of the words per year, you can select options on the graph to specify a certain month 
 
-}
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const BarChart = ({ data }) => {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const width = 1000;
+    const height = 400;
+    const svg = d3.select(svgRef.current)
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('preserveAspectRatio', 'xMidYMid meet');
+  
+    // Set up scales
+    const xScale = d3.scaleBand()
+      .domain(data.map(d => d.word))
+      .range([0, width])
+      .padding(0.1);
+  
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.frequency)])
+      .range([height, 0]);
+  
+    // Create and style the bars
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.word))
+      .attr('y', d => yScale(d.frequency))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => height - yScale(d.frequency))
+      .attr('fill', 'steelblue');
+  
+    // Add axes
+    svg.append('g')
+      .call(d3.axisBottom(xScale))
+      .attr('transform', `translate(0, ${height})`);
+  
+    svg.append('g')
+      .call(d3.axisLeft(yScale));
+  }, [data]);
+
+
+  return (
+    <div className="word-frequency-chart">
+      <svg ref={svgRef}></svg>
+    </div>
+  );
+};
 
 export default BarChart;
+  
--- optus-ai/src/Home.js
+++ optus-ai/src/Home.js
@@ -1,6 +1,9 @@
 import React  from 'react';
 import Fab from '@mui/material/Fab';
 import './Home.css';
+import BarChart from './Chart';
+//import * as data1 from '../../analysis/2011_frequencies/january.json';
+import * as data1 from './january.json';
 
 const Home = () => {
   const [showViz1, setShowViz1] = React.useState(false);
@@ -10,9 +13,10 @@ const Home = () => {
     setShowViz1(!showViz1);
   }
   const handleViz2 = () => {
+    console.log(data1);
     setShowViz2(!showViz2);
   }
-
+  const dataArray1 = Object.entries(data1).map(([word, frequency]) => ({ word, frequency }));
   return (
     <div className='background'>
       <header className="title">
@@ -20,13 +24,16 @@ const Home = () => {
         <p>OPiate Tracker Using Social Media Signals and AI</p>
       </header>
       <div className="buttons">
-        <Fab onClick={handleViz1} variant="extended" size="large" aria-label="add" style={{width: '200px', height: '50px', marginRight: '10px'}}> 
+        <Fab onClick={handleViz1} variant="extended" size="large" aria-label="add" 
+        style={{width: '200px', height: '50px', marginRight: '10px'}}> 
           Visualization 1
         </Fab>
-        <Fab onClick={handleViz2} variant="extended" size="large" aria-label="add" style={{width: '200px', height: '50px'}}>
+        <Fab onClick={handleViz2} variant="extended" size="large" aria-label="add" 
+        style={{width: '200px', height: '50px'}}>
           Visualization 2
         </Fab>
       </div>
+      {showViz1 && <BarChart data={dataArray1} />} {/* Conditionally render the BarChart */}
     </div>
   )
 } 
